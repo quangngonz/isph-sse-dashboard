a82ae631c218e85bde1f102ddaf0edb0108eb00e
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2025-02-04 01:04:48 +0700

    Implemented KanBan Board

--- src/pages/changelog/changelog.tsx
+++ src/pages/changelog/changelog.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import ChangelogTable from "./components/changelogTable/ChangelogTable";
 import SuggestionModal from './components/suggestionModal/suggestionModal';
 import SuggestionBar from "./components/SuggestionBar/suggestionBar";
+import KanbanBoard from "./components/kanbanBoard/KanbanBoard";
 
 const Changelog = () => {
     const [open, setOpen] = React.useState(false);
@@ -10,10 +11,11 @@ const Changelog = () => {
     const handleOpen = () => {
         setOpen(true);
     }
-    
+
     return (
         <div>
             <SuggestionBar handleOpen={handleOpen} success={success} setSuccess={setSuccess}/>
+            <KanbanBoard/>
             <ChangelogTable/>
             <SuggestionModal open={open} setOpen={setOpen} setSuccess={setSuccess}/>
         </div>
